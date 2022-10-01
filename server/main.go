f0668d4510b3baf8ce54f74c6db8aa31503d3966
Jimmy Fagerholm <jimmy@identio.fi>
2022-10-01 11:05:02 +0300

    part-1: solution

--- server/main.go
+++ server/main.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 )
 
 func main() {
+	chat := newChat()
+	go chat.run()
+
 	http.HandleFunc("/", helloWorldHandler)
-	// http.HandleFunc("/users", usersHandler)
+	http.HandleFunc("/users", usersHandler)
+	http.HandleFunc("/register", registerUserHandler)
+	http.HandleFunc("/unregister", unregisterUserHandler)
+	http.HandleFunc("/chat", func(w http.ResponseWriter, r *http.Request) {
+		serveWebsocket(chat, w, r)
+	})
 
 	log.Printf("Starting server on port 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
@@ -39,6 +50,69 @@ func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(message{Message: "Hello World!"})
 }
 
-// TODO: type user
+type user struct {
+	Name string `json:"name"`
+	ID   string `json:"id"`
+}
 
-// var userList []users
+var userList = []user{
+	{Name: "John Doe", ID: "1"},
+	{Name: "Jane Doe", ID: "2"},
+}
+
+func usersHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(userList)
+}
+
+func registerUserHandler(w http.ResponseWriter, r *http.Request) {
+	var newUser user
+	json.NewDecoder(r.Body).Decode(&newUser)
+
+	if r.Method != "POST" || newUser.Name == "" {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+	// generate random ID, add length of userList to ensure uniqueness
+	newUser.ID = strconv.Itoa(rand.Intn(1000000) + len(userList))
+
+	// append user to userList
+	userList = append(userList, newUser)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, `{ "id": "%s" }`, newUser.ID)
+}
+
+func unregisterUserHandler(w http.ResponseWriter, r *http.Request) {
+	var usr user
+	json.NewDecoder(r.Body).Decode(&usr)
+
+	for i, u := range userList {
+		if u.ID == usr.ID {
+			userList = append(userList[:i], userList[i+1:]...)
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprint(w, `{ "message": "user unreigistered" }`)
+			return
+		}
+	}
+
+	http.Error(w, `{ "message": "user not found"}`, http.StatusNotFound)
+}
+
+func serveWebsocket(chat *Chat, w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+
+	// check if user is registered
+	for _, u := range userList {
+		if u.ID == id {
+			chat.connectUser(u, w, r)
+			return
+		}
+	}
+
+	http.Error(w, `{ "message": "user not found"}`, http.StatusNotFound)
+}
